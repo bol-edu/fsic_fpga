7676288ee8045da4400b8a30bb7e856ff74562e5
zack <zack@zack.com>
2023-09-25 21:25:54 +0800

    remove fifo in AA module axis_master, axis_slave, axi_ctrl_logic is modified by Zack axilite_slave is modified by Willy

--- rtl/user/axilite_axis/rtl/axis_master.sv
+++ rtl/user/axilite_axis/rtl/axis_master.sv
@@ -6,151 +6,12 @@
 //      CREATED: 2023/06/16
 ///////////////////////////////////////////////////////////////////////////////
 
-module axi_fifo#(WIDTH=8'h8, DEPTH=8'h8)(
-    input wire clk,
-    input wire rst_n,
-    input wire wr_vld,
-    input wire rd_rdy,
-    input wire hack, // ???????????????
-    input wire clear,
-    input wire [WIDTH - 1:0] data_in,
-    output logic [WIDTH - 1:0] data_out,
-    output logic out_wr_rdy,
-    output logic rd_vld,
-    output logic last
-);
-
-    logic [WIDTH - 1:0] fifo [DEPTH - 1:0];
-    logic [7:0] wr_pointer, rd_pointer;
-    logic [7:0] wr_count, rd_count, wr_count_pre;
-    logic empty, full, sync_rd_vld, near_full, wr_rdy;
-
-    // pointer and water level, use valid and ready to handshake
-    always_ff@(posedge clk or negedge rst_n)begin
-        if(~rst_n)begin
-            wr_pointer <= 8'h0;
-            rd_pointer <= 8'h0;
-            wr_count <= 8'h0;
-            rd_count <= 8'h0;
-            wr_count_pre <= 8'h0;
-        end
-        else begin
-            if(clear)begin
-                wr_pointer <= 8'h0;
-                rd_pointer <= 8'h0;
-                wr_count <= 8'h0;
-                rd_count <= 8'h0;
-                wr_count_pre <= 8'h0;
-            end
-            if(rd_rdy && rd_vld && wr_rdy && wr_vld)begin // do read and write
-                rd_pointer <= (rd_pointer == (DEPTH - 8'b1)) ? 8'h0 : rd_pointer + 8'b1;
-                wr_pointer <= (wr_pointer == (DEPTH - 8'b1)) ? 8'h0 : wr_pointer + 8'b1;
-                fifo[wr_pointer] <= data_in;
-                wr_count <= wr_count + 8'b1;
-                rd_count <= rd_count + 8'b1;
-            end
-            else if(rd_rdy && rd_vld)begin // do read
-                rd_pointer <= (rd_pointer == (DEPTH - 8'b1)) ? 8'h0 : rd_pointer + 8'b1;
-                rd_count <= rd_count + 8'b1;
-            end
-            else if(wr_rdy && wr_vld)begin // do write
-                wr_pointer <= (wr_pointer == (DEPTH - 8'b1)) ? 8'h0 : wr_pointer + 8'b1;
-                wr_count <= wr_count + 8'b1;
-                fifo[wr_pointer] <= data_in;
-            end
-
-            wr_count_pre <= wr_count; // to fix bug in last transaction no ready
-        end
-    end
-
-    // output current read data if fifo is not empty, data go first before pointer moving, to solve read fifo cost too many clock
-    always_comb begin // use combinational so rd_pointer change reflect instantly
-        //data_out = '0; // initialize packed array (as a vector) with all zero
-
-        if(empty == 1'b0)begin
-            data_out = fifo[rd_pointer];
-        end
-        else
-            data_out = '0; // initialize packed array (as a vector) with all zero
-    end
-
-    // to fix bug in short transaction only have one clock data
-    always_ff@(posedge clk or negedge rst_n)
-        if(~rst_n)  sync_rd_vld <= 1'b0;
-        else        sync_rd_vld <= rd_vld;
-
-    // decide when this fifo can be read or wrote
-    always_comb begin
-        //wr_rdy = 1'b0;
-        //rd_vld = 1'b0;
-
-        if(hack)begin // for axis_master
-            if((wr_count_pre == wr_count) && (wr_count - rd_count) == 8'h2)begin // last data (n-1), raise last, axis_master can decide the bus behavior it should do
-                rd_vld = 1'b1;
-                last = 1'b1;
-            end
-            else if((wr_count_pre == wr_count) && (wr_count - rd_count) <= 8'h1)begin // last data (n)
-                rd_vld = 1'b0;
-                last = 1'b1;
-            end
-            else if((wr_count - rd_count) > 8'h1)begin // normal data, not last
-                rd_vld = 1'b1;
-                last = 1'b0;
-            end
-            else if(wr_count == 8'h1 && rd_count == 8'h0 && ~sync_rd_vld)begin // for short transaction only have one clock data
-                rd_vld = 1'b1;
-                last = 1'b0;
-            end
-            else begin
-                rd_vld = 1'b0;
-                last = 1'b0;
-            end
-        end
-        else begin // for control_logic
-            if((wr_count - rd_count) > 8'h1)begin
-                rd_vld = 1'b1;
-                last = 1'b0;
-            end
-            else if((wr_count - rd_count) == 8'h1)begin
-                rd_vld = 1'b1;
-                last = 1'b0;
-            end
-            else begin
-                rd_vld = 1'b0;
-                last = 1'b0;
-            end
-        end
-
-        if(full == 1'b0) // can be wrote
-            wr_rdy = 1'b1;
-        else
-            wr_rdy = 1'b0;
-
-        if(near_full == 1'b0) // reserve fifo to fix if two consecutive write will fail
-            out_wr_rdy = 1'b1;
-        else
-            out_wr_rdy = 1'b0;
-    end
-
-    always_comb begin
-        empty = ((wr_count - rd_count) == 8'h0);
-        near_full = ((wr_count - rd_count) >= DEPTH - 8'h2); // reserve fifo to fix if two consecutive write will fail
-        full = ((wr_count - rd_count) == DEPTH); // real full
-    end
-
-endmodule
-
-
 module axis_master(
     // backend source to trigger the axis master transaction
-    input wire bk_start,
     input wire [31:0] bk_data,
-    input wire [3:0] bk_tstrb,
-    input wire [3:0] bk_tkeep,
-    //input wire [1:0] bk_tid,
     input wire [1:0] bk_user,
-    output logic bk_nordy,
-    output logic bk_done,
+    input wire bk_valid,
+    output logic bk_ready,
 
     // frontend - axis master
     input wire axi_aclk,
@@ -160,231 +21,86 @@ module axis_master(
     output logic [3:0] axis_tstrb,
     output logic [3:0] axis_tkeep,
     output logic axis_tlast,
-    //output logic [1:0] axis_tid,
     output logic [1:0] axis_tuser,
     input wire axis_tready
 );
 
     // backend interface
-    // aclk       _/-\_/-\_/-\_/-\_/-\_/-\_/-\_/-\_/-\_/-\
-    // bk_start   _____/--------------\___________________
-    // bk_data    _____XXXXXXXXXXXXXXXX___________________
-    // bk_tstrb   _____XXXXXXXXXXXXXXXX___________________
-    // bk_tkeep   _____XXXXXXXXXXXXXXXX___________________
-    // bk_user    _____XXXXXXXXXXXXXXXX___________________
-    // bk_nordy   _________________/------\_______________
-    // bk_done    _________________________________/-\____
+    // aclk        _/-\_/-\_/-\_/-\_/-\_
+    // bk_data     _____XXXXXXXXXXXXXX__
+    // bk_user     _____XXXXXXXXXXXXXX__
+    // bk_valid    _____/-------------\_
+    // bk_ready    _________________/-\_
 
-    // axis_tlast _________________________________/-\____
-
-    logic enough_data, next_data;
-    logic [7:0] no_rdy_count;
-    logic [31:0] fifo_out_tdata;
-    logic [3:0] fifo_out_tstrb, fifo_out_tkeep;
-    //logic [1:0] fifo_out_tid, fifo_out_user;
-    logic [1:0] fifo_out_user;
-
-    parameter AXI_FIFO_DEPTH = 8'd8, AXI_FIFO_WIDTH = 8'd42, AXIS_RDY_TIMEOUT = 8'd5;
-    logic fifo_wr_vld, fifo_wr_rdy, fifo_rd_vld, fifo_rd_rdy, fifo_clear, fifo_last;
-    logic [41:0] fifo_data_in, fifo_data_out;
+    // axis_tready _______________/-\___
 
     // FSM state
-    enum logic [2:0] {AXIS_WAIT_DATA, AXIS_SEND_DATA, AXIS_SEND_LAST} axis_state, axis_next_state;
+    enum logic [2:0] {AXIS_WAIT_BACKEND, AXIS_SEND_DATA, AXIS_SEND_BK_RDY} axis_state, axis_next_state;
 
-    // FSM state, sequential logic, axis
+    // FSM state, sequential logic
     always_ff@(posedge axi_aclk or negedge axi_aresetn)begin
         if(~axi_aresetn)begin
-            axis_state <= AXIS_WAIT_DATA;
+            axis_state <= AXIS_WAIT_BACKEND;
         end
         else begin
             axis_state <= axis_next_state;
         end
     end
 
-    //assign enough_data = fifo_rd_vld;
-    // sync fifo_rd_vld, sequential logic
-    always_ff@(posedge axi_aclk or negedge axi_aresetn)begin
-        if(~axi_aresetn)begin
-            enough_data <= 1'b0;
-        end
-        else begin
-            enough_data <= fifo_rd_vld;
-        end
-    end
-
-    // FSM state, combinational logic, axis
+    // FSM state, combinational logic
     always_comb begin
         axis_next_state = axis_state;
 
         case(axis_state)
-            AXIS_WAIT_DATA:
-                if(enough_data)begin
+            AXIS_WAIT_BACKEND:
+                if(bk_valid)begin
                     axis_next_state = AXIS_SEND_DATA;
                 end
             AXIS_SEND_DATA:
-                if(fifo_last && ~enough_data)begin // one cycle data
-                    axis_next_state = AXIS_SEND_LAST;
-                end
-                else if(fifo_last && axis_tready)begin // last data
-                    axis_next_state = AXIS_SEND_LAST;
-                end
-            AXIS_SEND_LAST:
                 if(axis_tready)begin
-                    axis_next_state = AXIS_WAIT_DATA;
+                    axis_next_state = AXIS_SEND_BK_RDY;
                 end
+            AXIS_SEND_BK_RDY:
+                axis_next_state = AXIS_WAIT_BACKEND;
             default:
-                axis_next_state = AXIS_WAIT_DATA;
+                axis_next_state = AXIS_WAIT_BACKEND;
         endcase
     end
 
-    // FSM state, combinational logic, axis, output control
-    always_comb begin
-        //axis_tvalid = 1'b0;
-        //axis_tdata = 32'h0;
-        //axis_tstrb = 4'h0;
-        //axis_tkeep = 4'h0;
-        //axis_tuser = 2'h0;
-        //next_data = 1'b0;
-
-        if(axis_state == AXIS_SEND_DATA && axis_next_state == AXIS_SEND_LAST && enough_data == 1'b1)begin // workaround for two cycle data
-            axis_tvalid = 1'b1;
-            axis_tdata = fifo_out_tdata;
-            axis_tstrb = fifo_out_tstrb;
-            axis_tkeep = fifo_out_tkeep;
-            axis_tuser = fifo_out_user;
-            if(axis_tready)begin
-                next_data = 1'b1;
-            end
-            else begin
-                next_data = 1'b0;
-            end
-        end
-        else if(axis_state == AXIS_SEND_DATA && axis_next_state == AXIS_SEND_LAST && enough_data == 1'b0)begin // workaround for one cycle data, do not drive bus
-            if(axis_tready)begin
-                next_data = 1'b1;
-            end
-            else begin
-                next_data = 1'b0;
-            end
-            axis_tvalid = 1'b0;
-            axis_tdata = 32'h0;
-            axis_tstrb = 4'h0;
-            axis_tkeep = 4'h0;
-            axis_tuser = 2'h0;
-        end
-        else if(axis_state == AXIS_SEND_DATA)begin // normal data
-            axis_tvalid = 1'b1;
-            axis_tdata = fifo_out_tdata;
-            axis_tstrb = fifo_out_tstrb;
-            axis_tkeep = fifo_out_tkeep;
-            axis_tuser = fifo_out_user;
-            if(axis_tready)begin
-                next_data = 1'b1;
-            end
-            else begin
-                next_data = 1'b0;
-            end
-        end
-        else if(axis_state == AXIS_SEND_LAST)begin // final data
-            axis_tvalid = 1'b1;
-            axis_tdata = fifo_out_tdata;
-            axis_tstrb = fifo_out_tstrb;
-            axis_tkeep = fifo_out_tkeep;
-            axis_tuser = fifo_out_user;
-            next_data = 1'b0;
+    // FSM state, sequential logic, output control
+    always_ff@(posedge axi_aclk or negedge axi_aresetn)begin
+        if(~axi_aresetn)begin
+            axis_tvalid <= 1'b0;
+            axis_tdata <= 32'b0;
+            axis_tuser <= 2'b0;
         end
         else begin
-            axis_tvalid = 1'b0;
-            axis_tdata = 32'h0;
-            axis_tstrb = 4'h0;
-            axis_tkeep = 4'h0;
-            axis_tuser = 2'h0;
-            next_data = 1'b0;
+            case(axis_next_state)
+                //AXIS_WAIT_BACKEND: // do nothing
+                AXIS_SEND_DATA: begin
+                    axis_tvalid <= 1'b1;
+                    axis_tdata <= bk_data;
+                    axis_tuser <= bk_user;
+                end
+                AXIS_SEND_BK_RDY:
+                    axis_tvalid <= 1'b0;
+            endcase
         end
     end
 
-    // count if axis_tready not come
-    always_ff@(posedge axi_aclk or negedge axi_aresetn)begin
-        if(~axi_aresetn)
-            no_rdy_count <= 8'h0;
-        else if(axis_state == AXIS_SEND_DATA)
-            if(axis_tready)
-                no_rdy_count <= 8'h0;
-            else
-                no_rdy_count <= no_rdy_count + 8'b1;
-        else
-            no_rdy_count <= 8'h0;
+    assign axis_tstrb = 4'b0;
+    assign axis_tkeep = 4'b0;
+    assign axis_tlast = 1'b1;
+
+    // FSM state, latch logic, output control
+    always_latch begin
+        case(axis_state)
+            AXIS_WAIT_BACKEND:
+                bk_ready = 1'b0;
+            //AXIS_SEND_DATA: // do nothing
+            AXIS_SEND_BK_RDY:
+                bk_ready = 1'b1;
+        endcase
     end
-
-    assign bk_nordy = (no_rdy_count >= AXIS_RDY_TIMEOUT) ? 1'b1 : 1'b0;
-
-    axi_fifo #(.WIDTH(AXI_FIFO_WIDTH), .DEPTH(AXI_FIFO_DEPTH)) fifo(
-        .clk(axi_aclk),
-        .rst_n(axi_aresetn),
-        .wr_vld(fifo_wr_vld),
-        .rd_rdy(fifo_rd_rdy),
-        .hack(1'b1),
-        .data_in(fifo_data_in),
-        .data_out(fifo_data_out),
-        .out_wr_rdy(fifo_wr_rdy),
-        .rd_vld(fifo_rd_vld),
-        .last(fifo_last),
-        .clear(fifo_clear));
-
-    // send backend data to fifo
-    always_comb begin
-        //fifo_data_in = '0;
-        //fifo_wr_vld = 1'b0;
-
-        if(bk_start)begin
-            fifo_data_in = {bk_data, bk_tstrb, bk_tkeep, bk_user};
-            fifo_wr_vld = 1'b1;
-        end
-        else begin
-            fifo_data_in = '0;
-            fifo_wr_vld = 1'b0;
-        end
-    end
-
-    // get data from fifo
-    always_comb begin
-        //{fifo_out_tdata, fifo_out_tstrb, fifo_out_tkeep, fifo_out_user} = '0;
-        //fifo_rd_rdy = 1'b0;
-        //fifo_clear = 1'b0;
-
-        if(axis_state == AXIS_SEND_DATA || axis_state == AXIS_SEND_LAST)begin
-            {fifo_out_tdata, fifo_out_tstrb, fifo_out_tkeep, fifo_out_user} = fifo_data_out;
-        end
-        else
-            {fifo_out_tdata, fifo_out_tstrb, fifo_out_tkeep, fifo_out_user} = '0;
-
-        if(next_data)begin // receive slave tready, can send next data
-            fifo_rd_rdy = 1'b1;
-        end
-        else
-            fifo_rd_rdy = 1'b0;
-
-        if(bk_done)begin // clear fifo when transaction done to fix bug
-            fifo_clear = 1'b1;
-        end
-        else
-            fifo_clear = 1'b0;
-    end
-
-    //assign bk_done = (axis_state == AXIS_SEND_DATA) & (axis_next_state == AXIS_WAIT_DATA);
-    //assign bk_done_glitch = (axis_state == AXIS_SEND_LAST) & (axis_next_state == AXIS_WAIT_DATA);
-
-    // sync bk_done, sequential logic
-    always_ff@(posedge axi_aclk or negedge axi_aresetn)begin
-        if(~axi_aresetn)
-            bk_done <= 1'b0;
-        else
-            bk_done <= (axis_state == AXIS_SEND_LAST) & (axis_next_state == AXIS_WAIT_DATA);
-    end
-
-    assign axis_tlast = (axis_state == AXIS_SEND_LAST);
-    //assign axis_tlast = bk_done; // send tlast if transaction done ???
 
 endmodule
-
-
--- rtl/user/axilite_axis/rtl/axis_slave.sv
+++ rtl/user/axilite_axis/rtl/axis_slave.sv
@@ -9,10 +9,7 @@
 module axis_slave(
     // backend source to receive the axis slave transaction
     output logic [31:0] bk_data,
-    output logic [3:0] bk_tstrb,
-    output logic [3:0] bk_tkeep,
     output logic [1:0] bk_user,
-    output logic bk_tlast,
     input wire bk_ready,
     output logic bk_valid,
 
@@ -29,24 +26,20 @@ module axis_slave(
 );
 
     // backend interface
-    // aclk       _/-\_/-\_/-\_/-\_/-\_/-\_/-\
-    // bk_data    _____XXXXXXXXXXXXXXXX_______
-    // bk_tstrb   _____XXXXXXXXXXXXXXXX_______
-    // bk_tkeep   _____XXXXXXXXXXXXXXXX_______
-    // bk_user    _____XXXXXXXXXXXXXXXX_______
-    // bk_tlast   __________________/-\_______
-    // bk_ready   _/-----------------------\__
-    // bk_valid   _____/--------------\_______
-
-    // axis_tlast ________________/-\_________
+    // aclk       _/-\_/-\_/-\_/-\_/-\_/-\_/-\_
+    // bk_data    _____XXXXXXXXXXXX____XXXXXXXX
+    // bk_user    _____XXXXXXXXXXXX____XXXXXXXX
+    // bk_valid   _____/----------\____/-------
+    // bk_ready   _____________/--\____________
 
     // FSM state
-    enum logic [2:0] {AXIS_WAIT_BACKEND, AXIS_OUTPUT_DATA} axis_state, axis_next_state;
+    enum logic [2:0] {AXIS_WAIT_FRONTEND, AXIS_WAIT_BACKEND, AXIS_SEND_TREADY} axis_state, axis_next_state;
+    //logic sync_tvalid;
 
     // FSM state, sequential logic, axis
     always_ff@(posedge axi_aclk or negedge axi_aresetn)begin
         if(~axi_aresetn)begin
-            axis_state <= AXIS_WAIT_BACKEND;
+            axis_state <= AXIS_WAIT_FRONTEND;
         end
         else begin
             axis_state <= axis_next_state;
@@ -58,83 +51,55 @@ module axis_slave(
         axis_next_state = axis_state;
 
         case(axis_state)
-            AXIS_WAIT_BACKEND:
-                if(bk_ready && axis_tvalid)begin
-                    axis_next_state = AXIS_OUTPUT_DATA;
-                end
-                //else if(bk_ready)begin
-                //    axis_next_state = AXIS_RECV_DATA;
-                //end
-            //AXIS_RECV_DATA:
-            //    if(bk_ready && axis_tvalid)begin
-            //        axis_next_state = AXIS_OUTPUT_DATA;
-            //    end
-            //    else begin
-            //        axis_next_state = AXIS_RECV_DATA;
-            //    end
-            AXIS_OUTPUT_DATA:
-                if(axis_tvalid && bk_ready)begin
-                    axis_next_state = AXIS_OUTPUT_DATA;
-                end
-                else begin
+            AXIS_WAIT_FRONTEND:
+                //if(~bk_valid && sync_tvalid)begin
+                if(~bk_valid && axis_tvalid)begin
                     axis_next_state = AXIS_WAIT_BACKEND;
                 end
+            AXIS_WAIT_BACKEND:
+                if(bk_ready)begin
+                    axis_next_state = AXIS_SEND_TREADY;
+                end
+            AXIS_SEND_TREADY:
+                axis_next_state = AXIS_WAIT_FRONTEND;
             default:
                 axis_next_state = AXIS_WAIT_BACKEND;
         endcase
     end
 
-    // FSM state, combinational logic, axis, output control
-    always_comb begin
-        //axis_tready = 1'b0;
+    // FSM state, sequential logic, output control
+    always_ff@(posedge axi_aclk or negedge axi_aresetn)begin
+        if(~axi_aresetn)begin
+            bk_valid <= 1'b0;
+            bk_data <= 32'b0;
+            bk_user <= 2'b0;
+        end
+        else begin
+            case(axis_next_state)
+                //AXIS_WAIT_FRONTEND: // do nothing
+                AXIS_WAIT_BACKEND:begin
+                    bk_valid <= 1'b1;
+                    bk_data <= axis_tdata;
+                    bk_user <= axis_tuser;
+                end
+                AXIS_SEND_TREADY:begin
+                    bk_valid <= 1'b0;
+                end
+            endcase
+        end
+    end
 
-        case(axis_next_state) // if axis_state, axis_tready will be one clock late than bk_ready, so the data on frontend will be accepted but actually do not enter fifo because bk_ready is low now
-            //AXIS_WAIT_BACKEND: // do nothing
-            //AXIS_RECV_DATA:begin
-            //    axis_tready = 1'b1;
-            //end
-            AXIS_OUTPUT_DATA:begin
-                axis_tready = 1'b1;
-            end
-            default:
+    // FSM state, latch logic, output control
+    always_latch begin // experiment, test if not assigned signal keep the value before
+        case(axis_state)
+            AXIS_WAIT_FRONTEND: 
                 axis_tready = 1'b0;
+            //AXIS_WAIT_BACKEND:  // do nothing
+            AXIS_SEND_TREADY:
+                axis_tready = 1'b1;
+            //default:
+            //    axis_tready = 1'b0;
         endcase
     end
 
-    // combinational logic, backend interface
-    always_comb begin
-        //bk_data = 32'h0;
-        //bk_tstrb = 4'h0;
-        //bk_tkeep = 4'h0;
-        //bk_user = 2'h0;
-        //bk_tlast = 1'b0;
-        //bk_valid = 1'b0;
-
-        if((axis_next_state == AXIS_OUTPUT_DATA))begin // normal case
-            bk_data = axis_tdata;
-            bk_tstrb = axis_tstrb;
-            bk_tkeep = axis_tkeep;
-            bk_user = axis_tuser;
-            bk_tlast = axis_tlast;
-            bk_valid = 1'b1;
-        end
-        /*else if(axis_state == AXIS_OUTPUT_DATA && axis_next_state == AXIS_WAIT_BACKEND && bk_ready == 1'b0)begin // bk_ready falling, but the data is valid. EXCEPTION: short transaction only one clock cycle data
-            bk_data = axis_tdata;
-            bk_tstrb = axis_tstrb;
-            bk_tkeep = axis_tkeep;
-            bk_user = axis_tuser;
-            bk_tlast = axis_tlast;
-            bk_valid = axis_tvalid; // solve short transaction
-        end*/
-        else begin
-            bk_data = 32'h0;
-            bk_tstrb = 4'h0;
-            bk_tkeep = 4'h0;
-            bk_user = 2'h0;
-            bk_tlast = 1'b0;
-            bk_valid = 1'b0;
-        end
-    end
-
 endmodule
-
