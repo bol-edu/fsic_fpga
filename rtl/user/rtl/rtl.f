72e1bdea4d8f7181bd6af7d2c1104347296c4259
TonyHo <tonyho722@gmail.com>
2023-09-26 16:30:28 +0800

    support 4 user projects

--- rtl/user/rtl/rtl.f
+++ rtl/user/rtl/rtl.f
@@ -9,6 +9,10 @@
 -f ../mprj_io/rtl/rtl.f
 
    ../user_subsys/rtl/user_subsys.all.v
+   ../user_subsys/user_prj/rtl/user_prj0.v
+   ../user_subsys/user_prj/rtl/user_prj1.v
+   ../user_subsys/user_prj/rtl/user_prj2.v
+   ../user_subsys/user_prj/rtl/user_prj3.v
 // -f ../user_subsys/rtl/rtl.f
 
 ../rtl/fsic.vo
