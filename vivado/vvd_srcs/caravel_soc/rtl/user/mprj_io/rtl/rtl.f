794ecbe027e07f1f0cb8baf834a97975ab442504
joshsu <joshsu@vianextech.com>
2023-10-23 17:17:10 +0800

    Update FSIC related files into vivado folder for building FSIC fgpa. Also update module name of FSIC MPRI_IO from MPRJ_IO to MPRJ_IOz, which name is conflict the module defined in mprj_io.v.
    Signed-off-by: joshsu <joshsu@vianextech.com>

---
+++ vivado/vvd_srcs/caravel_soc/rtl/user/mprj_io/rtl/rtl.f
@@ -0,0 +1,1 @@
+../rtl/mprj_io.sv
---
+++ vivado/vvd_srcs/caravel_soc/rtl/user/rtl/rtl.f
@@ -0,0 +1,19 @@
+-f ../axilite_axis/rtl/rtl.f
+-f ../axis_switch/rtl/rtl.f
+-f ../config_ctrl/rtl/rtl.f
+-f ../fsic_clkrst/rtl/rtl.f
+-f ../io_serdes/rtl/rtl.f
+// ../logic_analyzer/rtl/logic_anlz.dummy_io.vd
+-f ../logic_analyzer/rtl/rtl.f
+// ../mprj_io/rtl/mprj_io.dummy_io.vd
+-f ../mprj_io/rtl/rtl.f
+
+   ../user_subsys/rtl/user_subsys.all.v
+   ../user_subsys/user_prj/rtl/user_prj0.v
+   ../user_subsys/user_prj/rtl/user_prj1.v
+   ../user_subsys/user_prj/rtl/user_prj2.v
+   ../user_subsys/user_prj/rtl/user_prj3.v
+// -f ../user_subsys/rtl/rtl.f
+
+../rtl/fsic.vo
+../rtl/user_project_wrapper.v
