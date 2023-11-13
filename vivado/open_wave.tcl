86da306a9c2fdf1addf413a1aeb4e643f726563a
joshsu <joshsu@vianextech.com>
2023-11-13 12:02:01 +0800

    Correcting the path of fsic_tb_behav.wdb.
    Signed-off-by: joshsu <joshsu@vianextech.com>

--- vivado/open_wave.tcl
+++ vivado/open_wave.tcl
@@ -1,2 +1,2 @@
-open_wave_database {./vvd_caravel_fpga_fsic_sim/vvd_caravel_fpga_fsic_sim.sim/sim_1/behav/xsim/fsic_tb_behav.wdb} 
+open_wave_database {./vvd_caravel_fpga_sim/vvd_caravel_fpga_sim.sim/sim_1/behav/xsim/fsic_tb_behav.wdb} 
 current_fileset
